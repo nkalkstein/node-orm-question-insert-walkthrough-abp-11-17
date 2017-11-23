dbf503d17fe5a4528f3f1e45809b692c19515555
Noah Kalkstein <nkalkstein@gmail.com>
2017-11-23 01:17:25 +0000

    Done.

--- models/Question.js
+++ models/Question.js
@@ -7,10 +7,10 @@ class Question{
         id INTEGER PRIMARY KEY,
         content TEXT
       )`
-      
+
       db.run(sql, function(){
         resolve("questions table created")
-      })      
+      })
     })
   }
 
@@ -18,6 +18,16 @@ class Question{
     this.content = content
   }
 
+  insert(){
+    const self = this // THIS IS THE CRUX
+    const sql = `INSERT INTO questions (content) VALUES (?)`
+    return new Promise(function(resolve){
+      db.run(sql, [self.content], function(err, result){
+        self.id = this.lastID
+        resolve(self)
+      })
+    })
+  }
 }
 
 module.exports = Question;
